a405aaebffe5387c19e97d2fe935781edf8e097f
alluong <45216428+alluong@users.noreply.github.com>
2018-11-28 15:15:08 -0800

    Create two-sum.py

---
+++ leetcode/python/two-sum.py
@@ -0,0 +1,28 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Example:
+
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        # empty complement dictionary
+        comp = {}
+        for index, val in enumerate(nums):
+            if target - val in comp:
+                # if complimentary is in comp{}, return
+                return comp[target - val], index
+            
+            # build up dictionary
+            comp[val] = index
