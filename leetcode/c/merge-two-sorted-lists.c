86eb6e4c347a0d36679712079c9126fc82508a59
alluong <45216428+alluong@users.noreply.github.com>
2018-11-28 15:49:19 -0800

    Create merge-two-sorted-lists.c

---
+++ leetcode/c/merge-two-sorted-lists.c
@@ -0,0 +1,48 @@
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing
+together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode *head, *p;
+    
+    if (l1 == NULL)
+        return l2;
+    if (l2 == NULL)
+        return l1;
+    if (l1->val < l2->val) {
+        head = l1;
+        l1 = l1->next;
+    } else {
+        head = l2;
+        l2 = l2->next;
+    }
+    p = head;
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->val < l2->val) {
+            p->next = l1;
+            p = p->next;
+            l1 = l1->next;
+        } else {
+            p->next = l2;
+            p = p->next;
+            l2 = l2->next;
+        }
+    }
+    if (l1 == NULL)
+        p->next = l2;
+    else
+        p->next = l1;
+    return head;
+}
