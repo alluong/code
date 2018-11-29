92d2b13e034d28ad7ccbb6da81591fa5414d91fe
alluong <45216428+alluong@users.noreply.github.com>
2018-11-28 17:23:58 -0800

    Delete list-comprehensions.py

--- list-comprehensions.py
+++
@@ -1,9 +0,0 @@
-a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-b = []
-
-#for num in range(len(a)):
-#    if (num % 2 == 0):
-#        b.append(a[num])
-b = [a[num] for num in range(len(a)) if num % 2 == 0]
-
-print(b)
