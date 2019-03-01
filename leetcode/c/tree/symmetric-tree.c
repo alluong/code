7b8459ce676abb2d5ad93cc117c7264b0eb25da5
alluong <45216428+alluong@users.noreply.github.com>
2019-02-28 21:00:10 -0800

    Create symmetric-tree.c

---
+++ leetcode/c/tree/symmetric-tree.c
@@ -0,0 +1,65 @@
+#if 0
+101. Symmetric Tree
+Easy
+
+1911
+
+43
+
+Favorite
+
+Share
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+
+Accepted
+358,772
+Submissions
+840,558
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool
+check_symmetric(struct TreeNode *left, struct TreeNode *right)
+{
+    /* if both are leaves, return true */
+    if (!left && !right)
+        return true;
+    
+    /* if only one is a leaf, return false */
+    if (!left || !right)
+        return false;
+    
+    /* if left val is same as right val, check the children */
+    return ((left->val == right->val) &&
+            check_symmetric(left->left, right->right) &&
+            check_symmetric(left->right, right->left));    
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if (!root)
+        return true;
+    return check_symmetric(root->left, root->right);    
+}
