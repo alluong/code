2ebd581d161cad586eb749ef82b745e7c98eeef3
alluong <45216428+alluong@users.noreply.github.com>
2019-02-28 20:58:56 -0800

    Create same-tree.c

---
+++ leetcode/c/tree/same-tree.c
@@ -0,0 +1,64 @@
+#if 0
+100. Same Tree
+Easy
+
+1002
+
+31
+
+Favorite
+
+Share
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+Accepted
+348,769
+Submissions
+706,267
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (!p && !q)
+        return true;
+    if (!p || !q)
+        return false;
+    return (p->val == q->val && isSameTree(p->left, q->left) &&
+           isSameTree(p->right, q->right));
+}
