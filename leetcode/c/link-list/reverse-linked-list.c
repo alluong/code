ddd57711533a44b6266fa6f8a941ac21d8ca9a7b
alluong <45216428+alluong@users.noreply.github.com>
2019-03-03 00:28:58 -0800

    Update reverse-linked-list.c

--- leetcode/c/link-list/reverse-linked-list.c
+++ leetcode/c/link-list/reverse-linked-list.c
@@ -32,3 +32,31 @@ struct ListNode* reverseList(struct ListNode* head) {
     }
     return p;
 }
+
+/* recursion implementation */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *rest;
+    
+    /* list is empty */
+    if (!head)
+        return NULL;
+    
+    /* list has 1 node */
+    if (!head->next)
+        return head;
+    
+    /* recursive call until reaching last node */
+    rest = reverseList(head->next);
+
+    /* head is the link node before "rest" since head->rest == "rest" */
+    head->next->next = head;
+    head->next = NULL;
+    return rest;
+}
