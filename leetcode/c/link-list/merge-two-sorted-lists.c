15ebee0cc37b4ae64c811731c61d6bd3812171ab
alluong <45216428+alluong@users.noreply.github.com>
2019-03-03 09:03:28 -0800

    Update merge-two-sorted-lists.c

--- leetcode/c/link-list/merge-two-sorted-lists.c
+++ leetcode/c/link-list/merge-two-sorted-lists.c
@@ -46,3 +46,27 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
         p->next = l1;
     return head;
 }
+
+/* recursive solution */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    if (!l1)
+        return l2;
+    if (!l2)
+        return l1;
+    if (l1->val < l2->val) {
+        /* link the next node on the smaller of l1->next or l2 */
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        /* link the next node on the smaller of l1 or l2->next */
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
